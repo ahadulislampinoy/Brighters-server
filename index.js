b5dfb8e3432cecd2b48020f9e5a351b5f5db158e
ahadulislampinoy <ahadulislampinoy@gmail.com>
2022-10-25 14:22:11 +0600

    categories done

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+const categories = require("./data/categories.json");
+const courses = require("./data/courses.json");
 
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Welcome To Brighters API!");
+});
+
+// sendig data when requested
+app.get("/categories", (req, res) => {
+  res.send(categories);
 });
 
 app.listen(port, () => {
